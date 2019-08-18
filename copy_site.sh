876497bc7eb19e74a6ebcf8c04f542c91478c5fb
Alain Lauzon <alauzon@alainlauzon.com>
2019-08-17 20:26:54 -0400

    Using the new variable names scheme

--- copy_site.sh
+++ copy_site.sh
@@ -3,7 +3,7 @@
 # HEADER
 #================================================================
 #% SYNOPSIS
-#+    ${SCRIPT_NAME} [-h] REPO_URL BRANCH_NAME USERNAME DB_NAME_PROD DB_USERNAME_PROD DB_PASSWORD_PROD HOME_PROD DB_NAME_DEV DB_USERNAME_DEV DB_PASSWORD_DEV HOME_DEV
+#+    ${SCRIPT_NAME} [-h] DRUPAL_REPO_URL BRANCH SITE SOURCE_ENV TARGET_ENV
 #%
 #% DESCRIPTION
 #%    Copy a site from prod to another one like dev or test.
@@ -17,6 +17,8 @@
 #================================================================
 # END_OF_HEADER
 #================================================================
+
+set -xe
 
 #== usage functions ==#
 SCRIPT_NAME=`basename $0`
@@ -99,22 +101,56 @@ shift $((${OPTIND} - 1)) ## shift options
 #  MAIN SCRIPT
 #============================
 
-if [ "$#" -ne 9 ]; then
+if [ "$#" -ne 5 ]; then
   usagefull
   exit 1
 fi
 
-REPO_URL=$1
-BRANCH_NAME=$2
-USERNAME=$3
-DB_NAME_PROD=$4
-DB_USERNAME_PROD=$5
-DB_PASSWORD_PROD=$6
-HOME_PROD=$7
-DB_NAME_DEV=$8
-DB_USERNAME_DEV=$8
-DB_PASSWORD_DEV=$10
-HOME_DEV=$11
+DRUPAL_REPO_URL=${1}
+BRANCH=${2}
+SITE=${3}
+SOURCE_ENV=${4}
+TARGET_ENV=${5}
+
+USERNAME_VARNAME = ${!SITE}_USERNAME
+USERNAME=${!USERNAME_VARNAME}
+
+SOURCE_HOME_VARNAME = ${!SITE}_${SOURCE_ENV}_HOME
+SOURCE_HOME=${!SOURCE_HOME_VARNAME}
+
+SOURCE_DB_NAME_VARNAME = ${!SITE}_${SOURCE_ENV}_DB_NAME
+SOURCE_DB_NAME=${!SOURCE_DB_NAME_VARNAME}
+
+SOURCE_DB_HOST_VARNAME = ${!SITE}_${SOURCE_ENV}_DB_HOST
+SOURCE_DB_HOST=${!SOURCE_DB_HOST_VARNAME}
+
+SOURCE_DB_PORT_VARNAME = ${!SITE}_${SOURCE_ENV}_DB_PORT
+SOURCE_DB_PORT=${!SOURCE_DB_PORT_VARNAME}
+
+SOURCE_DB_USERNAME_VARNAME = ${!SITE}_${SOURCE_ENV}_DB_USERNAME
+SOURCE_DB_USERNAME=${!SOURCE_DB_USERNAME_VARNAME}
+
+SOURCE_DB_PASSWORD_VARNAME = ${!SITE}_${SOURCE_ENV}_DB_PASSWORD
+SOURCE_DB_PASSWORD=${!SOURCE_DB_PASSWORD_VARNAME}
+
+TARGET_HOME_VARNAME = ${!SITE}_${TARGET_ENV}_HOME
+TARGET_HOME=${!SOURCE_HOME_VARNAME}
+
+TARGET_DB_NAME_VARNAME = ${!SITE}_${TARGET_ENV}_DB_NAME
+TARGET_DB_NAME=${!TARGET_DB_NAME_VARNAME}
+
+TARGET_DB_HOST_VARNAME = ${!SITE}_${TARGET_ENV}_DB_HOST
+TARGET_DB_HOST=${!TARGET_DB_HOST_VARNAME}
+
+TARGET_DB_PORT_VARNAME = ${!SITE}_${TARGET_ENV}_DB_PORT
+TARGET_DB_PORT=${!TARGET_DB_PORT_VARNAME}
+
+TARGET_DB_USERNAME_VARNAME = ${!SITE}_${TARGET_ENV}_DB_USERNAME
+TARGET_DB_USERNAME=${!TARGET_DB_USERNAME_VARNAME}
+
+TARGET_DB_PASSWORD_VARNAME = ${!SITE}_${TARGET_ENV}_DB_PASSWORD
+TARGET_DB_PASSWORD=${!TARGET_DB_PASSWORD_VARNAME}
+
 
 rndstr=`head /dev/urandom | tr -dc A-Za-z0-9 | head -c 13 ; echo ''`
 
@@ -122,16 +158,18 @@ rndstr=`head /dev/urandom | tr -dc A-Za-z0-9 | head -c 13 ; echo ''`
 mkdir /tmp/$rndstr
 
 # Cd to prod
-cd $HOME_PROD
+cd $SOURCE_HOME/public_html
 
 # Dump prod database
 /usr/local/bin/drush --yes sql-dump > /tmp/${rndstr}/sql-dump-prod.sql
 
 # Cd to dev
-cd $HOME_DEV
+cd $TARGET_HOME
+rm -Rf * .g* .h* .l*
 
 # Get code from git repo
-git clone $REPO_URL $BRANCH_NAME
+git clone --single-branch --branch $BRANCH $DRUPAL_REPO_URL .
+chown -R $USERNAME:$USERNAME .
 
 # CD to public_html
 cd public_html
@@ -140,26 +178,28 @@ cd public_html
 # The dev DB must already exists
 # @todo Is there a better way to do that?
 echo "SELECT concat('DROP TABLE IF EXISTS ', table_name, ';')  FROM information_schema.tables  WHERE table_schema = 'user_dev';"\
-  | /usr/local/bin/drush --root=. --db-url=mysql://$DB_USERNAME_DEV:$DB_PASSWORD_DEV@localhost/$DB_NAME_DEV sql-cli\
+  | /usr/local/bin/drush --root=. --db-url=mysql://$TARGET_DB_USERNAME:$TARGET_DB_PASSWORD@localhost/$TARGET_DB_NAME sql-cli\
   | tail -n +2\
-  | /usr/local/bin/drush --root=. --db-url=mysql://$DB_USERNAME_DEV:$DB_PASSWORD_DEV@localhost/$DB_NAME_DEV sql-cli
+  | /usr/local/bin/drush --root=. --db-url=mysql://$TARGET_DB_USERNAME:$TARGET_DB_PASSWORD@localhost/$TARGET_DB_NAME sql-cli
 
 # Create settings.php file
-rm -f $HOME_DEV/sites/default/settings.php
-/usr/local/bin/drush --db-url=mysql://$DB_USERNAME_DEV:$DB_PASSWORD_DEV@localhost/$DB_NAME_DEV site-install -y
-chown user:user $HOME_DEV/sites/default/settings.php
+rm -f sites/default/settings.php
+/usr/local/bin/drush site-install --force --db-url=mysql://$TARGET_DB_USERNAME:$TARGET_DB_PASSWORD@localhost/$TARGET_DB_NAME -y
+chown $USERNAME:$USERNAME sites/default/settings.php
 
 # Copy prod DB to dev DB
-/usr/local/bin/drush --root=. --db-url=mysql://$DB_USERNAME_DEV:$DB_PASSWORD_DEV@localhost/$DB_NAME_DEV sql-cli < /tmp/${rndstr}/sql-dump-prod.sql
+/usr/local/bin/drush --root=. --db-url=mysql://$TARGET_DB_USERNAME:$TARGET_DB_PASSWORD@localhost/$TARGET_DB_NAME sql-cli < /tmp/${rndstr}/sql-dump-prod.sql
 
 # Cleanup the temporary transfer directory
 rm -Rf /tmp/${rndstr}
 
 # Remove files in dev
-rm -rf $HOME_DEV/sites/default/files
+chmod +w sites/default sites/default/files
+rm -rf sites/default/files
 
 # Copy files and private directories from prod to dev
-cp -rp $HOME_PROD/sites/default/files $HOME_DEV/sites/default/
+cp -rp $SOURCE_HOME/public_html/sites/default/files sites/default/
+chmod -w sites/default
 
 # Clear cache dev
 /usr/local/bin/drush cc all
